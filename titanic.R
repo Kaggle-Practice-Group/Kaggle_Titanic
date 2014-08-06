a56247747a0016ffab65d89d53ae527bb14afe89
Kendall Parks <kendall.parks@live.com>
2014-08-05 22:30:46 -0500

    - Added a third partition to allow for testing and validation sets. - Added stacked model. - Added all predictors back in (for the time being)
    Achieved a real out-of-sample accuracy rate of 78.1%.

--- titanic.R
+++ titanic.R
@@ -1,10 +1,13 @@
 Mac = F
 Windows = T
+skipTraining = F
+ignorecores = 1 # Number of cores left free when training.
 
-data <- read.csv("data/train.csv", header=T, 
-                    colClasses=c("integer", "factor", "factor", "character", 
+data = read.csv("data/train.csv", header=T, 
+                   colClasses=c("integer", "factor", "factor", "character", 
                                  "factor", "numeric", "integer", "integer", 
                                  "character", "numeric", "character", "factor"))
+final.test = read.csv("data/test.csv", header=T)
 
 ## Set seed
 set.seed(3846)
@@ -20,18 +23,20 @@ levels(data$Survived) = c("N", "Y")
 levels(data$Sex) = c("F", "M")
 
 # Remove Name, Ticket, and Cabin
-new.data <- data[, c(-4, -9, -11)]
+new.data <- data[complete.cases(data), c(-4, -9, -11)]
 
 ###
 # Partition Data
 ###
 
-# Create data partition with 90% training data.
-indexSample <- createDataPartition(y=new.data$Survived, p=.9, list=FALSE)
+# Create data partition with 50% training data, 25% validation data, and 25% test data.
+indexSample <- createDataPartition(new.data$Survived, p=.5, list=FALSE)
 training <- new.data[indexSample,]
 attach(training)
 validation <- new.data[-indexSample,]
-validation <- na.omit(validation)
+validationIndices = createDataPartition(validation$Survived, p=.5, list=FALSE)
+testing = validation[-validationIndices,]
+validation = validation[validationIndices,]
 
 ###
 # Parameter Pruning
@@ -41,55 +46,54 @@ validation <- na.omit(validation)
 glm = glm(Survived ~ Fare + Embarked + Pclass + Sex + Age + SibSp + Parch, 
           data=training, family="binomial")
 aic = step(glm, direction="both")
-
-if(T) {
+print(summary(aic))
 
 ###
 # Training
 ###
 
+
 if(Mac) {
    # Set up parallel processors for Mac
    library('doMC')
-   ignorecores = 1 # Number of cores to NOT dedicate.
-   registerDoMC(cores = detectCores() - ignorecores) # set number of CPU cores
-}
-else if(Windows) {
+   registerDoMC(cores = detectCores() - ignorecores)
+} else if(Windows) {
    # Set up parallel processors for Windows
    library(doParallel)
-   ignorecores = 1 # Number of cores to NOT dedicate.
    cl = makeCluster(detectCores() - ignorecores)
    registerDoParallel(cl)
 }
 
-begin = Sys.time()
-
-# 1- Trees (rpart method)
-message("Training model (rpart)...")
-fitTREE <- train(Survived ~ SibSp + Age + Pclass + Sex, data=training, method='rpart', preProcess=c("center", "scale"))
-
-# 2- Random Forest
-message("Training model (Random Forests)...")
-fitControlRF <- trainControl(method = "cv", number = 15)
-fitRF <- train(Survived ~ SibSp + Age + Pclass + Sex, data=training, method='rf', preProcess=c("center", "scale"), 
-               trControl = fitControlRF, prox=TRUE, verbose = FALSE)
-
-# 3- GBM
-message("Training model (GBM)...")
-fitControl <- trainControl(method = "repeatedcv", number = 10, repeats = 20)
-fitGBM <- train(Survived ~ SibSp + Age + Pclass + Sex, data = training, method = 'gbm', trControl = fitControl, 
-                verbose = FALSE)
-
-# 4- Support Vector Machine (SVM)
-message("Training model (SVM)...")
-fitSVM <- train(Survived ~ SibSp + Age + Pclass + Sex, data=training, method = 'svmLinear', trControl = fitControl)
-
-end = Sys.time()
-message(sprintf("Total time to fit models, with %d cores: %d minutes %d seconds.", 
-                detectCores() - ignorecores, 
-                floor(as.numeric(end-begin, units="mins")), 
-                floor(as.numeric(end-begin, units="secs")) %% 60))
-
+if(!skipTraining) {
+   begin = Sys.time()
+   
+   # 1- Trees (rpart method)
+   message("Training model (rpart)...")
+   fitTREE <- train(Survived ~ Age + Pclass + Sex + Embarked + Parch + Fare + SibSp, data=training, method='rpart', preProcess=c("center", "scale"))
+   
+   # 2- Random Forest
+   message("Training model (Random Forests)...")
+   fitControlRF <- trainControl(method = "cv", number = 15)
+   fitRF <- train(Survived ~ Age + Pclass + Sex + Embarked + Parch + Fare + SibSp, data=training, method='rf', preProcess=c("center", "scale"), 
+                  trControl = fitControlRF, prox=TRUE, verbose = FALSE)
+   
+   # 3- GBM
+   message("Training model (GBM)...")
+   fitControl <- trainControl(method = "repeatedcv", number = 10, repeats = 20)
+   fitGBM <- train(Survived ~ Age + Pclass + Sex + Embarked + Parch + Fare + SibSp, data = training, method = 'gbm', trControl = fitControl, 
+                   verbose = FALSE)
+   
+   # 4- Support Vector Machine (SVM)
+   message("Training model (SVM)...")
+   fitSVM <- train(Survived ~ Age + Pclass + Sex + Embarked + Parch + Fare + SibSp, data=training, method = 'svmLinear', trControl = fitControl)
+   
+   end = Sys.time()
+   message(sprintf("Total time to fit models, with %d cores: %d minutes %d seconds.", 
+                   detectCores() - ignorecores, 
+                   floor(as.numeric(end-begin, units="mins")), 
+                   floor(as.numeric(end-begin, units="secs")) %% 60))
+}
+   
 ###
 # Cross-validation
 ###
@@ -152,20 +156,29 @@ declare <- rbind(declare, paste('Highest out-sample accuracy: ',
                                 *100, '% with method ', as.vector(errors.df[which.max(errors.df$outSample.Acc), 1]),
                                 '.', sep=''))
 cat(declare)
-}
 
 ###
-# Predict test cases
+# Prediction
 ###
-if(F) {
-   test <- read.csv("data/test.csv", header=T, 
-                    colClasses=c("integer", "factor", "character", 'factor', 
-                                 'numeric', 'integer', 'integer', 'character',
-                                 'numeric', 'character', 'factor'))
-   test <- test[, c(-3, -8, -10)]
-   levels(test$Survived) = c("N", "Y")
-   levels(test$Sex) = c("F", "M")
+if(!skipTraining) {   
+   # Combine predicted values with validation answers.
+   modelPreds = data.frame(tree=predictedValuesTREE, rf=predictedValuesRF, gbm=predictedValuesGBM, svm=predictedValuesSVM, Survived=validation$Survived)
    
-   predictedTEST <- predict(fitRF, test)
-   predictedTEST
+   # Create stacked model on predictions.
+   message("Training stacked model...")
+   stackedModel = train(Survived ~ ., method="gbm", data=modelPreds)
 }
+
+# Predict on validation set.
+stackedPreds = predict(stackedModel, modelPreds)
+
+# Predict on test set.
+tree = predict(fitTREE, testing)
+rf = predict(fitRF, testing)
+gbm = predict(fitGBM, testing)
+svm = predict(fitSVM, testing)
+testPreds = data.frame(tree, rf, gbm, svm)
+combinedPreds = predict(stackedModel, testPreds)
+
+# Print accuracy.
+cat(sprintf("Real out-of-sample prediction accuracy: %.01f%%\n", sum(combinedPreds == testing$Survived) / length(testing$Survived) * 100))
