ca84e95e92b88008019a472f6acbe6a461e418b2
Kendall Parks <kendall.parks@live.com>
2014-08-07 14:29:46 -0500

    I consolidated some more code and added a switch to turn the validation set on and off.
    - Created boolean 'use.validation' which automatically allows for use of a validation set, or not.
    
    - When 'use.validation' is TRUE, data is split accordingly: 45% training, 30% validation, & 25% testing. When it is false: 75% training, & 25% testing.
    
    - Fixed a bug where ANN in-sample accuracy was being incorrectly reported.
    
    - All models' in-sample accuracy is now dynamically calculated, and requires less code.
    
    - The formula used for training has now been pulled out into its own variable, so it can now be set once, and applied to all models.

--- titanic.R
+++ titanic.R
@@ -1,5 +1,9 @@
+# Clear environments.
+#rm(list=ls(), inherits=T) 
+
 skipTraining = F
 ignorecores = 2 # Number of cores left free when training.
+use.validation = F
 
 source("activateParallel.R")
 activateParallel(ignorecores)
@@ -12,7 +16,7 @@ data = read.csv("data/train.csv", header=T,
 final.test = read.csv("data/test.csv", header=T)
 
 # Set seed.
-set.seed(3846)
+#set.seed(3846)
 
 # Load libraries.
 library(caret)
@@ -27,26 +31,26 @@ levels(data$Survived) = c("N", "Y")
 levels(data$Sex) = c("F", "M")
 
 # Remove Name, Ticket, Cabin, and incomplete cases.
-new.data <- data[complete.cases(data), c(-4, -9, -11)]
+data <- data[complete.cases(data), c(-4, -9, -11)]
 
 ###
 # Partition Data
 ###
 
-# Create data partition with 50% training data, 25% validation data, and 25% test data.
-indexSample <- createDataPartition(new.data$Survived, p=.5, list=FALSE)
-training <- new.data[indexSample,]
-attach(training)
-validation <- new.data[-indexSample,]
-validationIndices = createDataPartition(validation$Survived, p=.5, list=FALSE)
-testing = validation[-validationIndices,]
-validation = validation[validationIndices,]
+# Split data: 75% training, 25% testing.
+inTrain <- createDataPartition(data$Survived, p=.75, list=FALSE)
+training <- data[inTrain,]
+testing = data[-inTrain,]
 
+# If using validation set, split data: 45% training, 30% validation, 25% testing.
+if(use.validation) {
+   inValidation = createDataPartition(training$Survived, p=.4, list=FALSE)   
+   validation = training[inValidation,]
+   training = training[-inValidation,]
+}
 ###
-# Parameter Pruning
+# Best Subsets Regression
 ###
-
-# Perform Best Subsets Regression.
 
 # Adjusted R2 method.
 lr2 = leaps(data.matrix(training[,3:9]), training[,2], method="adjr2")
@@ -63,31 +67,35 @@ print(lcp$which[lcp$Cp <= 8,])
 ###
 
 if(!skipTraining) {   
+   
+   # Define formula to use for training.
+   formula = formula(Survived ~ Age, Pclass + Sex + SibSp)
+   
    begin = Sys.time()
    
    # 1- Trees (rpart method)
    message("Training model (rpart)...")
-   fitTREE <- train(Survived ~ Age + Pclass + Sex + SibSp, data=training, method='rpart', preProcess=c("center", "scale"))
+   fitTREE <- train(formula, data=training, method='rpart', preProcess=c("center", "scale"))
    
    # 2- Random Forest
    message("Training model (Random Forests)...")
    fitControlRF <- trainControl(method = "cv", number = 30)
-   fitRF <- train(Survived ~ Age + Pclass + Sex + SibSp, data=training, method='rf', preProcess=c("center", "scale"), 
+   fitRF <- train(formula, data=training, method='rf', preProcess=c("center", "scale"), 
                   trControl = fitControlRF, prox=TRUE, verbose = FALSE)
    
    # 3- GBM
    message("Training model (GBM)...")
    fitControl <- trainControl(method = "repeatedcv", number = 10, repeats = 20)
-   fitGBM <- train(Survived ~ Age + Pclass + Sex + SibSp, data=training, method='gbm', trControl=fitControl, 
+   fitGBM <- train(formula, data=training, method='gbm', trControl=fitControl, 
                    verbose = FALSE)
    
    # 4- Support Vector Machine (SVM)
    message("Training model (SVM)...")
-   fitSVM <- train(Survived ~ Age + Pclass + Sex + SibSp, data=training, method='svmLinear', trControl=fitControl)
+   fitSVM <- train(formula, data=training, method='svmLinear', trControl=fitControl)
    
    # 5- Neural Net
    message("Training model (Neural Net)...")
-   fitANN = train(Survived ~ Age + Pclass + Sex + SibSp, data=training, method='avNNet')
+   fitANN = train(formula, data=training, method='avNNet')
    
    end = Sys.time()
    message(sprintf("Total time to fit models, with %d cores: %d minutes %d seconds.", 
@@ -105,34 +113,33 @@ if(!skipTraining) {
 }
    
 ###
-# 
+#  Predicting With Original Models
 ###
 
 # Predict on each dataset with the original models. (Not sure yet which of these we're going to need)
 train.predict = data.frame(lapply(models, predict, training), training$Survived)
 
-valid.predict = data.frame(lapply(models, predict, validation), validation$Survived)
-
 test.predict = data.frame(lapply(models, predict, testing), testing$Survived)
 
-names(train.predict) = names(valid.predict) = names(test.predict) = c(method, "Survived")
+names(train.predict) = names(test.predict) = c(method, "Survived")
 
-# Calculate in-sample accuracy for each model.
-inSample.Accuracy <- c(max(fitTREE$results[,2]), 
-                       max(fitRF$results[,2]), 
-                       max(fitGBM$results[,4]),
-                       max(fitSVM$results[,2]),
-                       max(fitANN$results[,2])
-)
-
+if(use.validation) {
+   valid.predict = data.frame(lapply(models, predict, validation), validation$Survived)
+   names(valid.predict) = c(method, "Survived")
+}
+   
 # Create confusion matrices for each model and store them in a list.
 confusion = lapply(train.predict[,-ncol(train.predict)], confusionMatrix, train.predict[,ncol(train.predict)])
 
+###
+# Accuracy
+###
+
+# Calculate in-sample accuracy for each model.
+inSample.Accuracy = sapply(models, function(x) {max(x$results$Accuracy)})
+
 # Estimate OOS accuracy for each model.
-outSample.Accuracy = NULL
-for(cm in confusion) {
-   outSample.Accuracy = c(outSample.Accuracy, cm$overall[1])
-}
+outSample.Accuracy = sapply(confusion, function(x) {x$overall["Accuracy"]})
 
 # Create errors data frame.
 errors.df <- data.frame(method = method, 
@@ -152,28 +159,16 @@ cat(sprintf("Highest in-sample accuracy: %.02f%% with method %s\r\nHighest estim
 ###
 # Prediction
 ###
-if(!skipTraining) {   
-   # Combine predicted values of models on the TRAINING set.
-#  modelPreds = data.frame(tree=predict(fitTREE, training), rf=predict(fitRF, training), 
-#                          gbm=predict(fitGBM, training), svm=predict(fitSVM, training), 
-#                           Survived=training$Survived)
-   
+if(!skipTraining) {      
    # Create stacked model on predictions.
    message("Training stacked model...")
    stackedModel = train(Survived ~ ., method="rf", data=train.predict)
 }
 
 # Predict on validation set.
-stacked.predict = predict(stackedModel, valid.predict)
-
-# Predict on test set.
-#tree = predict(fitTREE, testing)
-#rf = predict(fitRF, testing)
-#gbm = predict(fitGBM, testing)
-#svm = predict(fitSVM, testing)
-#testPreds = data.frame(tree, rf, gbm, svm)
-#combinedPreds = predict(stackedModel, testPreds)
+#stacked.predict = predict(stackedModel, valid.predict)
 
 # Print accuracy.
-cat(sprintf("Real out-of-sample prediction accuracy on validation set: %.01f%%\n", 
-            sum(stacked.predict == valid.predict$Survived) / length(valid.predict$Survived) * 100))
+#cat(sprintf("Real out-of-sample prediction accuracy on validation set: %.01f%%\n", 
+#            sum(stacked.predict == valid.predict$Survived) / length(valid.predict$Survived) * 100))
+
