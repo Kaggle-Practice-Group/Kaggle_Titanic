14b8f36eeac82292d8a0bfc38a5a2011eb86d1ae
Lemur03 <noel.amael@gmail.com>
2014-08-17 13:39:26 -0300

    Extract title from names

--- cleaningdata.R
+++ cleaningdata.R
@@ -2,15 +2,68 @@
 rm(list=ls(), inherits=T) 
 
 # Read files.
-data = read.csv("data/raw/train.csv", header=T, 
-                colClasses=c("integer", "factor", "factor", "character", 
-                             "factor", "numeric", "integer", "integer", 
-                             "character", "numeric", "character", "factor"))
-
+data = read.csv("data/raw/train.csv")
+                #, header=T, 
+                #colClasses=c("integer", "factor", "integer", "character", 
+                #             "factor", "numeric", "integer", "integer", 
+                #             "character", "numeric", "character", "factor"))
+#names(data)
 
 final.test = read.csv("data/raw/test.csv")
 
-names(final.test)
+
+# Cleaning Names
+#Replace Mlle to Miss
+data$Name<- gsub("Mlle","Miss",data$Name)
+final.test$Name <- gsub("Mlle","Miss",final.test$Name)
+
+# Replace Countess to Lady
+data$Name<- gsub("Countess","Lady",data$Name)
+final.test$Name <- gsub("Countess","Lady",final.test$Name)
+
+# Replace Mme to Mrs
+data$Name<- gsub("Mme","Mrs",data$Name)
+final.test$Name <- gsub("Mme","Mrs",final.test$Name)
+
+# Replace Jonkheer to Miss
+data$Name<- gsub("Jonkheer","Miss",data$Name)
+final.test$Name <- gsub("Jonkheer","Miss",final.test$Name)
+
+# Replace Dona to Mrs
+data$Name<- gsub("Dona","Lady",data$Name)
+final.test$Name <- gsub("Dona","Lady",final.test$Name)
+
+# Replace Ms to Mrs
+data$Name<- gsub("Ms","Miss",data$Name)
+final.test$Name <- gsub("Ms","Miss",final.test$Name)
+
+# Replace Don to Sir
+data$Name<- gsub("Don","Sir",data$Name)
+final.test$Name <- gsub("Don","Sir",final.test$Name)
+
+#Add title to train
+data<-cbind(data,data$Name)
+names(data) <- c(names(data)[1:(dim(data)[2]-1)],"Title")
+data$Title <- as.character(data$Title)
+
+for (i in 1:dim(data)[1])
+{
+  tmp <- strsplit(data$Name[i]," ")[[1]]  
+  data$Title[i]<-tmp[grep("[A-z][a-z]*\\.",tmp)]
+}
+
+#Add title to test
+final.test<-cbind(final.test,final.test$Name)
+names(final.test) <- c(names(final.test)[1:(dim(final.test)[2]-1)],"Title")
+final.test$Title <- as.character(final.test$Title)
+
+for (i in 1:dim(final.test)[1])
+{
+  tmp <- strsplit(final.test$Name[i]," ")[[1]]  
+  final.test$Title[i]<-tmp[grep("[A-z][a-z]*\\.",tmp)]
+}
+
+
 ###
 # Pre-processing
 ###
@@ -18,35 +71,34 @@ names(final.test)
 #aggregate(Survived~Embarked,subset(data,Survived=="Y"),length)
 data[which(data$Embarked==''),]$Embarked = "C"
 
+data$Survived <- as.factor(data$Survived)
 levels(data$Survived) = c("N", "Y")
+data$Sex <- as.factor(data$Sex)
 levels(data$Sex) = c("F", "M")
 
-
+final.test$Sex <- as.factor(final.test$Sex)
 levels(final.test$Sex) = c("F", "M")
-final.test$Pclass = as.factor(final.test$Pclass)
 
 source("partitionData.R")
 source("activateParallel.R")
 activateParallel(0)
 partitionData(na.omit(data),"Age",F)
-
+unique(data$Title)
 set.seed(1245)
-# Define formula to use for training.
-formula = formula(Age ~ Fare^2+ Pclass + SibSp + Parch + Embarked)
-fitControlRF <- trainControl(method = "cv", number =75)
-fitRF <- train(formula, data=training, method='rf',
-               trControl = fitControlRF,prox=T  ,verbose = FALSE)
-ggplot(fitRF)
 
+# Define formula to use for training.
+formula = formula(Age ~ Fare+Title+ Pclass + SibSp +Parch )
+fitControlRF <- trainControl(method = "cv", number =25)
+fitRF <- train(formula, data=training, method='rf',
+               trControl = fitControlRF,prox=T  ,verbose = FALSE,importance=T)
+ggplot(fitRF)
+varImp(fitRF)
 
 missClass = function(values,prediction){sum((prediction/values-1)>0.20)/length(values)}
 missClass(testing$Age,predict(fitRF, testing))
 
 data[which(is.na(data$Age)),]$Age <- round(predict(fitRF, data[which(is.na(data$Age)),]))
-
 final.test[which(is.na(final.test$Age)),]$Age <- round(predict(fitRF, final.test[which(is.na(final.test$Age)),]))
-
-#summary(data)
 
 
 # Remove  Name, Ticket, Cabin, and incomplete cases.
